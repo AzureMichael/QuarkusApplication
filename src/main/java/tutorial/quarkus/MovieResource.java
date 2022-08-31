945e72de6f40ea48d375325a5c6412e06c477da4
Stefanescu Mihai <Mihai.Stefanescu@msg.group>
2022-08-31 19:24:34 +0300

    Added simple REST endpoint

---
+++ src/main/java/tutorial/quarkus/MovieResource.java
@@ -0,0 +1,71 @@
+package tutorial.quarkus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.jboss.resteasy.annotations.Query;
+
+@Path("/movies")
+public class MovieResource {
+
+    public static List<String> movies = new ArrayList<>();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getMovies() {
+        return Response.ok(movies).build();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/size")
+    public Integer countMovies() {
+        return movies.size();
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response createMovie (String newMovie){
+        movies.add(newMovie);
+        return Response.ok(movies).build();
+    }
+
+    @PUT
+    @Path("/{movieToUpdate}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response updateMovie(
+        @PathParam("movieToUpdate") String movieToUpdate,
+        @QueryParam("movie") String updateMovie) {
+        movies = movies.stream().map(movie -> {
+            if(movie.equals(movieToUpdate)){
+                return updateMovie;
+            }
+            return movie;
+        }).collect(Collectors.toList());
+
+        return Response.ok(movies).build();
+    }
+
+    @DELETE
+    @Path("{movieToDelete}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response deleteMovie(
+        @PathParam("movieToDelete") String movieToDelete){
+        boolean removed = movies.remove(movieToDelete);
+        return removed ? Response.noContent().build() :
+               Response.status(Response.Status.BAD_REQUEST).build();
+    }
+}
